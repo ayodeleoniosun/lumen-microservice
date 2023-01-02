75274e55b70640e32ffe984f4569f7f403df4bd9
Ayodele Oniosun <heywire4u@gmail.com>
2023-01-02 12:51:23 +0100

    chore: create controller and routes

---
+++ authorsApi/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Return list of authors
+     *
+     * @return Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create new author
+     *
+     * @return Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * get and show details of existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+
+    public function show($author)
+    {
+
+    }
+
+    /**
+     * Update an existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+
+    public function update(Request $request, $author)
+    {
+
+    }
+
+    /**
+     * Remove an existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+
+    public function destroy($author)
+    {
+
+    }
+}
